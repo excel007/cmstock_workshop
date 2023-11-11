75a12cd623101677b053fc747c5bddc71feec418
excel007 <eak.ekkachai@gmail.com>
2023-11-12 01:28:15 +0700

    theming

--- app/_components/common/AuthProvider.tsx
+++ app/_components/common/AuthProvider.tsx
@@ -1,15 +1,21 @@
 "use client"
 import { useRouter, usePathname } from 'next/navigation'
-import React from 'react'
+import React, { useEffect } from 'react'
 
 type Props = { children: React.ReactNode }
 
 export default function AuthProvider({ children }: Props) {
     const router = useRouter();
     const path = usePathname();
-    if (path == "/") {
-        router.push("/stock");
-        return null;
+    useEffect(() => {
+        initialize()
+    })
+
+    const initialize = () => {
+        if (path == "/") {
+            router.push("/stock");
+            return null;
+        }
     }
     return <div>{children}</div>
 }
