cb6264de851cd6a038ca80be45520a0ec77d3b05
excel007 <eak.ekkachai@gmail.com>
2023-11-14 12:36:31 +0700

    after Yup

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -5,23 +5,32 @@ import EmailIcon from '@mui/icons-material/Email';
 
 import React, { useState } from 'react'
 import { useForm, SubmitHandler, Controller } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
+import { useRouter } from 'next/navigation';
 
-interface User  {
+interface User {
   username: string;
   password: string;
 }
 
 type Props = {}
 
-export default function Register({ }: Props) {
+export default function Login({ }: Props) {
   const initialValue: User = { username: "", password: "" }
-  
+  const formValidateSchema = yup.object().shape({
+    username: yup.string().required("Username is required!!!").trim(),
+    password: yup.string().required("Password is required!!!").trim(),
+  })
   const {
     control,
     handleSubmit,
     formState: { errors },
-  } = useForm<User>({ defaultValues: initialValue })
-
+  } = useForm<User>({
+    defaultValues: initialValue,
+    resolver: yupResolver(formValidateSchema)
+  })
+  const router = useRouter()
 
   const showForm = () => {
     return (
@@ -30,45 +39,48 @@ export default function Register({ }: Props) {
           alert(JSON.stringify(value))
         })}
       >
-  {/* username */ }
-  < Controller
-name = "username"
-control = { control }
-render = {({ field }) => (
-  < TextField
-    {...field}
-    error={true}
-    helperText={""}
-    id="username"
-    label="username"
-    autoComplete='email'
-    InputProps={{
-      startAdornment: (
-        <InputAdornment position="start">
-          <EmailIcon />
-        </InputAdornment>
-      ),
-    }
-    }
-    variant="outlined"
-    margin='normal'
-    fullWidth
-    autoFocus
-  />
-)}
+        {/* username */}
+        < Controller
+          name="username"
+          control={control}
+          render={({ field }) => (
+            < TextField
+              {...field}
+              error={Boolean(errors.username?.message)}
+              helperText={errors.username?.message?.toString()}
+              id="username"
+              label="username"
+              autoComplete='email'
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <EmailIcon />
+                  </InputAdornment>
+                ),
+              }
+              }
+              variant="outlined"
+              margin='normal'
+              fullWidth
+              autoFocus
+            />
+          )}
 
-/>
+        />
 
-{/* password */ }
+        {/* password */}
         <Controller
           name="password"
           control={control}
           render={({ field }) => (
             <TextField
               {...field}
+              error={Boolean(errors.password?.message)}
+              helperText={errors.password?.message}
               id="password"
               label="Password"
               autoComplete='password'
+              type='password'
               InputProps={{
                 startAdornment: (
                   <InputAdornment position="start">
@@ -90,32 +102,34 @@ render = {({ field }) => (
           variant='contained'
           color='primary'
         >
-          Create Account
+          Login
         </Button>
         <Button
           className='mt-4'
           type='button'
           fullWidth
           variant='outlined'
-          onClick={() => { }}
+          onClick={() => {
+            router.push("/register")
+          }}
         >
-          Cancel
+          Register
         </Button>
 
       </form >
     );
   };
 
-return (
-  <Box className='flex justify-center items-center'>
-    <Card elevation={7} className='max-w-[345px] mt-10'>
-      <CardContent>
-        <Typography gutterBottom variant='h5' component='h2'>
-          Register
-        </Typography>
-        {showForm()}
-      </CardContent>
-    </Card>
-  </Box>
-)
+  return (
+    <Box className='flex justify-center items-center'>
+      <Card elevation={7} className='max-w-[345px] mt-10'>
+        <CardContent>
+          <Typography gutterBottom variant='h5' component='h2'>
+            Login
+          </Typography>
+          {showForm()}
+        </CardContent>
+      </Card>
+    </Box>
+  )
 }
